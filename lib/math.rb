03469a6d0585ee5ac99fa7dcdb3d565b1503131a
HyoSung Bidol-Lee <hyosung11@me.com>
2019-06-08 16:00:26 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,34 @@
 def addition(num1, num2)
-
+  addition = num1 + num2
 end
+num1 = 1
+num2 = 3
 
 def subtraction(num1, num2)
-
+  subtraction = num1 - num2
 end
+num1 = 2
+num2 = 4
 
 def division(num1, num2)
-
+  division = num1 / num2
 end
+num1 = 2
+num2 = 4
 
 def multiplication(num1, num2)
-
+  multiplication = num1 * num2
 end
+num1 = 2
+num2 = 4
 
 def modulo(num1, num2)
-
+  modulo = num1 % num2
 end
+num1 = 2
+num2 = 4
 
 def square_root(num)
-
+  square_root = num**
 end
+num = 3
